502272a9368f4056cbb69e5ad8bcfecca768b768
Thomas Scott <38812486+thomasscott95@users.noreply.github.com>
2022-02-01 19:15:55 +0000

    Player Movement update
    movement, rotation and jump

---
+++ paint-prototype/Assets/Fork/Tom/Scripts/PlayerCamera.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerCamera : MonoBehaviour
+{
+    [SerializeField] private float sensitivityX;
+    [SerializeField] private float sensitivityY;
+
+    Camera cam;
+
+    float mouseX;
+    float mouseY;
+
+    float multiplier = 0.01f;
+
+    float rotationX;
+    float rotationY;
+
+
+    // Start is called before the first frame update
+    void Start() {
+        cam = GetComponentInChildren<Camera>();
+
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+    }
+
+    // Update is called once per frame
+    void Update() {
+        myInput();
+
+        cam.transform.localRotation = Quaternion.Euler(rotationX, 0, 0);
+        transform.rotation = Quaternion.Euler(0,rotationY,0);
+    }
+    void myInput() {
+        mouseX = Input.GetAxisRaw("Mouse X");
+        mouseY = Input.GetAxisRaw("Mouse Y");
+
+        rotationY += mouseX * sensitivityX * multiplier;
+        rotationX -= mouseY * sensitivityY * multiplier;
+
+        rotationX = Mathf.Clamp(rotationX, -90f,90f);
+    }
+}
---
+++ paint-prototype/Assets/Fork/Tom/Scripts/PlayerMovement.cs
@@ -0,0 +1,85 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    // Start is called before the first frame update
+    [Header("Movement")]
+    public float movementSpeed = 6f;
+    public float movementMultiplier = 15f;
+    [SerializeField] float airMovementMultiplier = 0.4f;
+
+
+
+    [Header("Keybinds")]
+    [SerializeField] KeyCode jump = KeyCode.Space;
+    public float jumpForce = 400;
+
+    [Header("Drag")]
+    float groundDrag = 6f;
+    float airDrag = 2f;
+
+    float playerHight = 2f;
+
+
+    float hMovement;
+    float vMovement;
+
+    bool isGrounded;
+
+    Vector3 direction;
+
+    Rigidbody rbody;
+
+    void Start() {
+        rbody = GetComponent<Rigidbody>();
+        rbody.freezeRotation = true;
+    }
+
+    // Update is called once per frame
+    void Update() {
+        myInput();
+        controlDrag();
+
+        isGrounded = Physics.Raycast(transform.position, Vector3.down, playerHight / 2 + 0.1f);
+        if (Input.GetKeyDown(jump) && isGrounded) {
+            Jump();
+        }
+        print(isGrounded);
+    }
+
+    void myInput() {
+        hMovement = Input.GetAxisRaw("Horizontal");
+        vMovement = Input.GetAxisRaw("Vertical");
+
+        direction = transform.forward * vMovement + transform.right * hMovement;
+    }
+
+    private void FixedUpdate(){
+        PlayerMoving();
+    }
+
+    void controlDrag() {
+        if (isGrounded) {
+            rbody.drag = groundDrag;
+        }
+        else {
+            rbody.drag = airDrag;
+        }
+    }
+
+    void PlayerMoving() {
+        if (isGrounded) {
+            rbody.AddForce(direction.normalized * movementSpeed * movementMultiplier, ForceMode.Acceleration);
+        }
+        else if (!isGrounded) {
+            rbody.AddForce(direction.normalized * movementSpeed * movementMultiplier * airMovementMultiplier, ForceMode.Acceleration);
+
+        }
+    }
+
+    void Jump() {
+        rbody.AddForce(transform.up * jumpForce, ForceMode.Acceleration);
+    }
+}
