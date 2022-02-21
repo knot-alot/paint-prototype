bed42a54c71e93e2404c792bf42a11b6a89cda36
Lauren <l.pugh@ncl.ac.uk>
2022-02-21 13:25:13 +0000

    paint fix

--- paint-prototype/Assets/Fork/Joe/Scripts/Player.cs
+++ paint-prototype/Assets/Fork/Joe/Scripts/Player.cs
@@ -33,15 +33,19 @@ public class Player : MonoBehaviour
 
         if (Input.GetButtonDown("UnlimPaint")) usePaint = !usePaint;
 
-        if (Input.GetMouseButton(0))
+        if (Input.GetMouseButtonDown(0))
         {
-            if(usePaint) TakeDamage(1);
             if (currentHealth > 0) {
                 praticle.Play();
             }
         
         }
-        if (Input.GetMouseButtonUp(0))
+        if (Input.GetMouseButton(0))
+        {
+            if (usePaint) TakeDamage(1);
+        }
+
+        else if (Input.GetMouseButtonUp(0))
         {
             praticle.Stop();
         }
