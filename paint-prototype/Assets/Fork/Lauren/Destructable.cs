ea2cf850f95fae131d9791bce2bc14663c576bb0
Lauren <l.pugh@ncl.ac.uk>
2022-02-21 14:07:02 +0000

    destructable objects

---
+++ paint-prototype/Assets/Fork/Lauren/Destructable.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Destructable : MonoBehaviour
+{
+    [SerializeField]
+    ParticleSystem deathParticle;
+
+    public int maxHealth = 100;
+    public int currentHealth;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        currentHealth = maxHealth;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (currentHealth == 0) Death();
+    }
+
+    public void TakeDamage(int damage)
+    {
+        currentHealth -= damage;
+
+        if (currentHealth < 0) currentHealth = 0;
+    }
+
+    void Death()
+    {
+        Instantiate(deathParticle, transform.position, Quaternion.identity);
+
+        Destroy(gameObject);
+    }
+}
