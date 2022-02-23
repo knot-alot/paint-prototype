703ff8e524ba58762551a9755573e9eb65242dae
joemcloughlin14 <j.mcloughlin2@newcastle.ac.uk>
2022-02-23 14:57:40 +0000

    merge conflicts

--- paint-prototype/Assets/Fork/Joe/Scripts/Player.cs
+++ paint-prototype/Assets/Fork/Joe/Scripts/Player.cs
@@ -9,8 +9,8 @@ public class Player : MonoBehaviour
     [SerializeField]
     ParticleSystem deathParticle;
     [SerializeField] Camera cam;
-    [SerializeField] private int maxHealth = 200;
-    private int currentHealth;
+    public int maxHealth = 1000;
+    public int currentHealth;
 
     public PaintTank paintTank;
 
@@ -18,47 +18,41 @@ public class Player : MonoBehaviour
 
     public bool usePaint = true;
     // Start is called before the first frame update
-
     void Start()
     {
-        this.currentHealth = this.maxHealth;
-        this.paintTank.SetMaxFill(this.maxHealth);
-
+        currentHealth = maxHealth;
+        paintTank.SetMaxFill(maxHealth);
+       
     }
 
     // Update is called once per frame
     void Update()
     {
 
-        Vector3 angle = this.cam.transform.localEulerAngles;
+        Vector3 angle = cam.transform.localEulerAngles;
 
-        if (Input.GetButtonDown("UnlimPaint")) this.usePaint = !this.usePaint;
+        if (Input.GetButtonDown("UnlimPaint")) usePaint = !usePaint;
 
         if (Input.GetMouseButtonDown(0))
         {
-            if (currentHealth > 0)
-            {
-                this.praticle.Play();
+            if(usePaint) TakeDamage(1);
+            if (currentHealth > 0) {
+                praticle.Play();
             }
-
+        
         }
-        if (Input.GetMouseButton(0))
+       else if (Input.GetMouseButtonUp(0))
         {
-            if (this.usePaint) this.TakeDamage(1);
+            praticle.Stop();
         }
 
-        else if (Input.GetMouseButtonUp(0))
-        {
-            this.praticle.Stop();
-        }
-
-
+        
 
         praticle.transform.localEulerAngles = angle;
 
-
-        text.SetActive(false);
-        Ray ray = this.cam.ScreenPointToRay(Input.mousePosition);
+    
+    text.SetActive(false);
+        Ray ray = cam.ScreenPointToRay(Input.mousePosition);
         RaycastHit hit;
         Interactable interactable = null;
 
@@ -66,45 +60,43 @@ public class Player : MonoBehaviour
         {
             interactable = hit.collider.GetComponent<Interactable>();
 
-            if (interactable) this.text.SetActive(true);
-
+            if (interactable) text.SetActive(true);
+          
         }
 
         if (Input.GetKey(KeyCode.E))
         {
-            if (interactable) this.AddPaint(1);
+            if (interactable) AddPaint(1);
         }
 
-        if (currentHealth == 0) this.PlayerDeath();
-
-    }
-
-
-    public void TakeDamage(int damage)
-    {
-        this.currentHealth -= damage;
-
-        if (this.currentHealth < 0) this.currentHealth = 0;
-
-        paintTank.SetFill(this.currentHealth);
+        if (currentHealth == 0) PlayerDeath();
         
     }
 
-    public void AddPaint(int paint)
+    void TakeDamage(int damage)
     {
-        this.currentHealth += paint;
+        currentHealth -= damage;
 
-        if (this.currentHealth > this.maxHealth) this.currentHealth = this.maxHealth;
+        if (currentHealth < 0) currentHealth = 0;
 
-        paintTank.SetFill(this.currentHealth);
+        paintTank.SetFill(currentHealth);
+    }
+
+    void AddPaint(int paint)
+    {
+        currentHealth += paint;
+
+        if (currentHealth > maxHealth) currentHealth = maxHealth;
+
+        paintTank.SetFill(currentHealth);
     }
 
     void PlayerDeath()
     {
-        Instantiate(this.deathParticle, this.transform.position, Quaternion.identity);
+        Instantiate(deathParticle, transform.position, Quaternion.identity);
 
-        Destroy(this.gameObject);
+        Destroy(gameObject);
     }
 
-
+  
 }
