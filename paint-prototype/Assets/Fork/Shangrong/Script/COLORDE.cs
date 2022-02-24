55d964bf6a0877038938d3924262c1c5cc4b2a63
tsr0099 <56968663+tsr0099@users.noreply.github.com>
2022-02-24 16:45:48 +0000

    COLOR DAMGE
    on enemy color will loss health

--- paint-prototype/Assets/Fork/Shangrong/Script/COLORDE.cs
+++ paint-prototype/Assets/Fork/Shangrong/Script/COLORDE.cs
@@ -4,8 +4,10 @@ using UnityEngine;
 
 public class COLORDE : MonoBehaviour
 {
+    private Color player1 = new Color(0.000f, 0.451f, 1.000f, 1.000f);
+    private Color player2 = new Color(0.000f, 0.4117f, 1.000f, 1.000f);
     //public GameObject obj;
-    public Camera cam;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -15,17 +17,18 @@ public class COLORDE : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        Vector3 position = new Vector3(2.75f,3f,0.25f);
+       
         // Vector3 position = this.GetComponent<Transform>().position;
-        Vector3 dir = new Vector3(0, -1, 0);
-        dir = transform.TransformDirection(Vector3.down) * 20;
-        Debug.DrawRay(position, dir, Color.green);
-   
-           // Vector3 position = Input.mousePosition;
-        
-            RaycastHit hit;
+      
+        Vector3 dir = transform.TransformDirection(Vector3.down);
 
-            if (Physics.Raycast(position,dir,out hit, 100.0f,8))
+
+        // Vector3 position = Input.mousePosition;
+        int layermask = 1 << 8;
+        layermask = ~layermask;
+        RaycastHit hit;
+
+            if (Physics.Raycast(transform.position,dir,out hit, 100.0f, layermask))
             {
                 
                 Renderer renderer = hit.collider.GetComponent<Paintable>().getRenderer();
@@ -44,16 +47,18 @@ public class COLORDE : MonoBehaviour
                 //   Texture2D texture2D1 = obj.GetComponent<MeshRenderer>().material.GetTexture("Texture2D_41271c3c5f484ca2a435c65087a81705") as Texture2D;
                 // Texture texture2D1 = obj.GetComponent<MeshRenderer>().material.GetTexture("_MaskTexture");
                   Vector2 pCoord = hit.textureCoord;
-                   transform.position = hit.point;
+                  
                     pCoord.x *= texture.width;
                        pCoord.y *= texture.height;
                 //   Vector2 tiling = renderer.material.GetTextureScale("_MaskTexture");
                  //  Vector2 tiling = obj.GetComponent<MeshRenderer>().material.GetTextureScale("Texture2D_41271c3c5f484ca2a435c65087a81705");
                  Color color = texture.GetPixel(Mathf.FloorToInt(pCoord.x), Mathf.FloorToInt(pCoord.y ));
-                // Debug.Log(hit.point.x + " " + hit.point.y);
-                 Debug.Log(color);
-
-            }
+            // Debug.Log(hit.point.x + " " + hit.point.y);
+         //   Debug.Log(color.g);
+           // Debug.Log(Mathf.Abs(player2.g-color.g)<= 0.0001f );
+            if (this.tag == "enemy"&& Mathf.Abs(player1.g - color.g) <= 0.0001f) this.GetComponent<enemy>().TakeDamage(1);
+            if (this.tag == "Player" && Mathf.Abs(player2.g - color.g) <= 0.0001f) this.GetComponent<Player>().TakeDamage(1);
+        }
         
     }
    
