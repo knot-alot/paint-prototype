76d4a3a7127bd1084cc062dfedf08098f157c357
Kingsley Hopking <39307332+BeardyKing@users.noreply.github.com>
2022-02-25 13:51:22 +0000

    timer

--- paint-prototype/Assets/Fork/Tom/Scripts/EnemyRespawnManager.cs
+++ paint-prototype/Assets/Fork/Tom/Scripts/EnemyRespawnManager.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class EnemyRespawnManager : MonoBehaviour
 {
@@ -26,23 +27,24 @@ public class EnemyRespawnManager : MonoBehaviour
 
     [SerializeField]
     GameObject camObj;
+    bool _isdirty = false;
+    [SerializeField] TextMeshPro text;
+
+
     // Start is called before the first frame update
     void Start()
     {
+       
         pl = this.GetComponent<enemy>();
         plMov = this.GetComponent<enemycontroller>();
         plCam = this.GetComponent<enemycamer>();
-        StartCoroutine(playerDeath());
+        runDeath();
     }
 
     public void runDeath()
     {
-        StartCoroutine(playerDeath());
-    }
-
-
-    IEnumerator playerDeath()
-    {
+        _isdirty = true;
+        time_remaing = respawnDelay;
         plMov.GetComponent<Rigidbody>().velocity = Vector3.zero;
         pl.praticle.Stop();
         pl.enabled = false;
@@ -51,21 +53,32 @@ public class EnemyRespawnManager : MonoBehaviour
 
         Vector3 deathPosition = gameObject.transform.position;
         gameObject.transform.position = respawnZone.transform.position;
-        Quaternion camRot = camObj.transform.rotation;
         camObj.transform.rotation = Quaternion.Euler(new Vector3(90, 0, 0));
 
         Instantiate(deathParticle, deathPosition, Quaternion.identity);
 
-        yield return new WaitForSeconds(respawnDelay);
-
-        pl.currentHealth = pl.maxHealth;
-        gameObject.transform.position = respawnPoint.transform.position;
-        camObj.transform.rotation = camRot;
-
-        plCam.enabled = true;
-        pl.enabled = true;
-        plMov.enabled = true;
-        pl.TakeDamage(1);
     }
+
+    float time_remaing;
+    private void Update(){
+        if (_isdirty){
+            time_remaing -= Time.deltaTime;
+            if (time_remaing <= 0){
+                _isdirty = false;
+                pl.currentHealth = pl.maxHealth;
+                gameObject.transform.position = respawnPoint.transform.position;
+
+                plCam.enabled = true;
+                pl.enabled = true;
+                plMov.enabled = true;
+                pl.TakeDamage(1);
+            }
+        }
+        text.text = ((int)time_remaing).ToString();
+    }
+
+
+
+
 
 }
--- paint-prototype/Assets/Fork/Tom/Scripts/RespawnManager.cs
+++ paint-prototype/Assets/Fork/Tom/Scripts/RespawnManager.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class RespawnManager : MonoBehaviour
 {
@@ -26,23 +27,23 @@ public class RespawnManager : MonoBehaviour
 
     [SerializeField]
     GameObject camObj;
+    bool _isdirty = false;
+    [SerializeField] TextMeshPro text;
+
     // Start is called before the first frame update
     void Start()
     {
+
         pl = this.GetComponent<Player>();
         plMov = this.GetComponent<Assassin>();
         plCam = this.GetComponent<PlayerCamera>();
-        StartCoroutine(playerDeath());
+        runDeath();
     }
 
     public void runDeath()
     {
-        StartCoroutine(playerDeath());
-    }
-
-
-    IEnumerator playerDeath()
-    {
+        _isdirty = true;
+        time_remaing = respawnDelay;
         plMov.GetComponent<Rigidbody>().velocity = Vector3.zero;
         pl.praticle.Stop();
         pl.enabled = false;
@@ -51,21 +52,33 @@ public class RespawnManager : MonoBehaviour
 
         Vector3 deathPosition = gameObject.transform.position;
         gameObject.transform.position = respawnZone.transform.position;
-        Quaternion camRot = camObj.transform.rotation;
-        camObj.transform.rotation = Quaternion.Euler(new Vector3(90,0,0));
+        camObj.transform.rotation = Quaternion.Euler(new Vector3(90, 0, 0));
 
         Instantiate(deathParticle, deathPosition, Quaternion.identity);
 
-        yield return new WaitForSeconds(respawnDelay);
-
-        pl.currentHealth = pl.maxHealth;
-        gameObject.transform.position = respawnPoint.transform.position;
-        camObj.transform.rotation = camRot;
-
-        plCam.enabled = true;
-        pl.enabled = true;
-        plMov.enabled = true;
-        pl.TakeDamage(1);
     }
+
+    float time_remaing;
+    private void Update()
+    {
+        if (_isdirty)
+        {
+            time_remaing -= Time.deltaTime;
+            if (time_remaing <= 0)
+            {
+                _isdirty = false;
+                pl.currentHealth = pl.maxHealth;
+                gameObject.transform.position = respawnPoint.transform.position;
+
+                plCam.enabled = true;
+                pl.enabled = true;
+                plMov.enabled = true;
+                pl.TakeDamage(1);
+            }
+            text.text = ((int)time_remaing).ToString();
+        }
+    }
+
+
 
 }
